35751cb51f5f6ca0bbea0f117a33460aa6c33860
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2024-07-10 23:00:45 +0400

    add prettier, postgresql, prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { VersioningType } from '@nestjs/common'
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+	app.enableCors({
+		credentials: true,
+		origin: 'http://localhost:3001',
+		exposedHeaders: ['set-cookie'],
+	})
+	app.setGlobalPrefix('api/')
+	app.enableVersioning({
+		type: VersioningType.URI,
+	})
+	app.use(cookieParser())
+
+	const config = new DocumentBuilder().setTitle('MYG').setVersion('1.0').build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('swagger', app, document)
+	await app.listen(4010)
 }
-bootstrap();
+bootstrap()
